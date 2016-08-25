f03e1d8ac994d130eac3177e10b5363edd7a4038
24437 <244373082@qq.com>
2016-08-25 11:32:38 +0800

    BActivity is ok

--- app/src/main/java/com/joyingnet/www/test/MainActivity.java
+++ app/src/main/java/com/joyingnet/www/test/MainActivity.java
@@ -2,12 +2,13 @@ package com.joyingnet.www.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"hahah",Toast.LENGTH_SHORT).show();
     }
 }
